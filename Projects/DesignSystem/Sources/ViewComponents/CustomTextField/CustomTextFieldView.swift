408a09fb9fdd7c79cb8d0461ee4ef63ce2b6bb9f
Виталий Багаутдинов <vitalijbagautdinov@MacBook-Air-Vitalij.local>
2025-08-25 13:47:00 +0700

    add CustonTextField

--- Projects/DesignSystem/Sources/ViewComponents/CustomTextField/CustomTextFieldView.swift
+++ Projects/DesignSystem/Sources/ViewComponents/CustomTextField/CustomTextFieldView.swift
@@ -8,12 +8,77 @@
 
 import SwiftUI
 
-struct CustomTextFieldView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+public struct CustomTextFieldView: View {
+    
+    public enum TextFieldType: String {
+        case username = "Username"
+        case password = "Password"
+        case confirmPassword = "Confirm Password"
+    }
+    
+    let type: TextFieldType
+    @Binding var text: String
+    
+    public init(type: TextFieldType, text: Binding<String>) {
+        self.type = type
+        self._text = text
+    }
+    
+    @State private var eye: Bool = false
+    
+    public var body: some View {
+        VStack(alignment: .leading) {
+            Text("\(type.rawValue):")
+                .padding(.trailing, 5)
+                .frame(minWidth: 80, alignment: .leading)
+            
+            textFieldWithEyeIfNeeded()
+        }
+        .padding(10)
+    }
+}
+
+extension CustomTextFieldView {
+    @ViewBuilder
+    private func textFieldWithEyeIfNeeded() -> some View {
+        if type == .password || type == .confirmPassword {
+            ZStack {
+                passwordTextField
+                eyeButton
+            }
+        } else {
+            TextField(type.rawValue, text: $text)
+                .padding()
+                .modifier(BottomBorderModifier(color: Asset.Colors.borderColor.swiftUIColor))
+        }
+    }
+
+    private var passwordTextField: some View {
+        Group {
+            if !eye {
+                SecureField(type.rawValue, text: $text)
+            } else {
+                TextField(type.rawValue, text: $text)
+            }
+        }
+        .padding()
+        .padding(.trailing, 30)
+        .modifier(BottomBorderModifier(color: Asset.Colors.borderColor.swiftUIColor))
+    }
+
+    private var eyeButton: some View {
+        HStack {
+            Spacer()
+            Button {
+                eye.toggle()
+            } label: {
+                Image(systemName: eye ? "eye.slash" : "eye")
+            }
+            .padding(.trailing, 10)
+        }
     }
 }
 
 #Preview {
-    CustomTextFieldView()
+    CustomTextFieldView(type: .username, text: .constant(""))
 }
