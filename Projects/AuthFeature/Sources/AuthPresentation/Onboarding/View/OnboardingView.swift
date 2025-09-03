a3aea552b9db0debb1e94e3c38bec68ece8a8f76
VadimBaby <vadimdim22@gmail.com>
2025-09-03 15:20:38 +0700

    some fix

--- Projects/AuthFeature/Sources/AuthPresentation/Onboarding/View/OnboardingView.swift
+++ Projects/AuthFeature/Sources/AuthPresentation/Onboarding/View/OnboardingView.swift
@@ -14,7 +14,7 @@ public struct OnboardingView: View {
     @State private var viewModel: OnboardingViewModel
     
     public init(viewModel: OnboardingViewModel) {
-        self.viewModel = viewModel
+        self._viewModel = State(wrappedValue: viewModel)
     }
     
     public var body: some View {
@@ -47,12 +47,12 @@ public struct OnboardingView: View {
                 Button(Strings.Button.signIn) {
                     //
                 }
-                .buttonStyle(PrimaryButtonStyle(style: .primary))
+                .primaryButton(style: .primary)
                 
                 Button(Strings.Button.createAccount) {
                     //
                 }
-                .buttonStyle(PrimaryButtonStyle(style: .secondary))
+                .primaryButton(style: .secondary)
             }
         }
         .padding(.horizontal, 24)
